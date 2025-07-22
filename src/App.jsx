ee511fc99d1417493509c27b4ea1ec6952d8e59c
salehchadi <DARKGHOSTOF2002@GMAIL.COM>
2025-07-22 22:23:58 +0300

    deleted some react files

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-r from-pink-500 to-yellow-400 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">✅ Tailwind via CDN is working!</h1>
-    </div>
-  );
-}
